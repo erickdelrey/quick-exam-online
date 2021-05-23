1a8033d0cece78e6ef7cff519c37ee34be06bb0c
ErickDelRey <erick.del.rey5@gmail.com>
2021-05-23 17:00:58 +0800

    commit

--- answer-exam.php
+++ answer-exam.php
@@ -189,33 +189,5 @@ if (isset($_POST['submitAnswer'])) {
             }
         ?>
     </main>
-    <script>
-        <?php echo $exam->getExamDuration()?>;
-        // set the date we're counting down to
-        //var target_date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0).getTime();
-
-        // variables for time units
-        var minutes, seconds;
-
-        // get tag element
-        var countdown = document.getElementById('countdown');
-
-        // update the tag with id "countdown" every 1 second
-        setInterval(function() {
-
-            // find the amount of "seconds" between now and target
-            var current_date = new Date().getTime();
-            var seconds_left = (target_date - current_date) / 1000;
-
-            // do some time calculations
-            minutes = parseInt(seconds_left / 60);
-            seconds = parseInt(seconds_left % 60);
-
-            // format countdown string + set tag value
-            countdown.innerHTML = '<span class="days">' + days + ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">' +
-                minutes + ' <label>Minutes</label></span> <span class="seconds">' + seconds + ' <label>Seconds</label></span>';
-
-        }, 1000);
-    </script>
 </body>
 </html>
